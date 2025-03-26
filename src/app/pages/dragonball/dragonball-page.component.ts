df52ddf3c23bb40f15458bd65ede61b213fa2e20
Daniel Forero <danielforero93@gmail.com>
2025-03-26 12:37:28 -0500

    for y ngClass

--- src/app/pages/dragonball/dragonball-page.component.ts
+++ src/app/pages/dragonball/dragonball-page.component.ts
@@ -1,6 +1,29 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, signal } from '@angular/core';
 
+interface Character {
+  id: number;
+  name: string;
+  power: number;
+}
 @Component({
   templateUrl: 'dragonball-page.component.html',
 })
-export class DragonballPageComponent {}
+export class DragonballPageComponent {
+  characters = signal<Character[]>([
+    {
+      id: 1,
+      name: 'Goku',
+      power: 15000,
+    },
+    {
+      id: 2,
+      name: 'VEgeta',
+      power: 9000,
+    },
+    {
+      id: 3,
+      name: 'Piccolo',
+      power: 8555,
+    },
+  ]);
+}
